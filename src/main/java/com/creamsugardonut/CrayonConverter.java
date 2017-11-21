7fabfea0a8e195d6917520b79713710be6d36722
lks21c <lks21c@gmail.com>
2017-11-21 12:29:19 +0900

    yes

--- src/main/java/com/creamsugardonut/CrayonConverter.java
+++ src/main/java/com/creamsugardonut/CrayonConverter.java
@@ -7,13 +7,29 @@ import java.io.IOException;
 
 public class CrayonConverter {
 
+    private static final CharSequence TAB = "    ";
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(
                 new FileReader(new File("week1.md")));
         String line;
+
+        StringBuilder sb = new StringBuilder();
+        boolean startCode = false;
         while ((line = reader.readLine()) != null) {
-            if(line.equals("    ")){
-                System.out.println("yes");
+            if (line.contains(TAB)) {
+                if (!startCode) {
+                    startCode = true;
+                }
+                sb.append(line.replace(TAB, "") + "\n");
+            } else {
+                if(startCode) {
+                    startCode = false;
+                    System.out.println();
+                    System.out.println("<pre class=\"lang:java\">" + sb.toString() + "</pre>");
+                    System.out.println();
+                    sb = new StringBuilder();
+                }
             }
         }
         reader.close();
