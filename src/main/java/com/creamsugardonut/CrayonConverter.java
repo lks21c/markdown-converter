4d11fd824617a77b492b0872ab1a3d274e1625cd
lks21c <lks21c@gmail.com>
2017-11-21 12:17:37 +0900

    Frist Commit

---
+++ src/main/java/com/creamsugardonut/CrayonConverter.java
@@ -0,0 +1,23 @@
+package com.creamsugardonut;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CrayonConverter {
+
+    public static void main(String[] args) throws IOException {
+        String str = "";
+        BufferedReader reader = new BufferedReader(
+                new FileReader(new File("week1.md")));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if(line.equals("    ")){
+                System.out.println("yes");
+            }
+        }
+        reader.close();
+    }
+}
